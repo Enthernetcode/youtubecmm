da2ee7b30cb4cbc69f1b50d856d17d2edb16e278
NAME <test@gmail.com>
2023-10-16 14:30:24 +0100

    .

--- getids.py
+++ getids.py
@@ -1,13 +1,22 @@
 from urllib.parse import urlparse, parse_qs
-
-youtube_link = input("Enter the YouTube video link: ")
-
-parsed_url = urlparse(youtube_link)
-
-video_id = parsed_url.path.split("/")[-1]
-
-query_params = parse_qs(parsed_url.query)
-channel_id = query_params.get("si", [""])[0].split("_")[-1]
-
-print("Video ID:", video_id)
-print("Channel ID:", channel_id)
+list = []
+links = [
+'https://youtu.be/75KjLd5XVMY?si=FwQfjZ9bLYAjhfnm',
+'https://youtu.be/75KjLd5XVMY?si=V1ZVS0ZFVPecRbmd'
+]
+for line in links:
+  print(line)
+  youtube_link = line
+  parsed_url = urlparse(youtube_link)
+  video_id = parsed_url.path.split("/")[-1]
+  query_params = parse_qs(parsed_url.query)
+  channel_id = query_params.get("si", [""])[0].split("_")[-1]
+  print("Video ID:", video_id)
+  print("Channel ID:", channel_id)
+  id = f'{channel_id}, {video_id}'
+  list.append(id)
+print (list)
+for line in list:
+ with open('videos.csv', 'a+') as l:
+   l.write(line + '\n')
+   l.close()
